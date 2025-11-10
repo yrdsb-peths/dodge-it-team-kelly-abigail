1784f13ea43777ac6089612df13ba3d923257359
kiwi0825 <nbeinniwllek@gmail.com>
2025-11-10 14:20:14 -0500

    Move Hero up and down

--- Hero.java
+++ Hero.java
@@ -1,19 +1,30 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Hero here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Hero extends Actor
 {
-    /**
-     * Act - do whatever the Hero wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    // position 0 = top, 1 = middle, 2 = bottom
+    int position = 0;
     public void act()
     {
-        // Add your action code here.
+        
+        if (Greenfoot.mouseClicked(null)) {
+            position++;
+            if (position > 2) {
+                // puts the hero back to the top
+                position = 0;
+            }
+        }
+        
+        if (position == 0) {
+            // top
+            setLocation(100, 60);
+        } else if (position == 1) {
+            // middle
+            setLocation(100, 200);
+        } else if (position == 2) {
+            // bottom
+            setLocation(100, 350);
+        }
+        
     }
 }
