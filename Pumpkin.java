fc9000437bca44a9a3de4a5a5765795a23b737b1
person-030 <abigailyang783@gmail.com>
2025-11-10 14:03:36 -0500

    move projectile

---
+++ Pumpkin.java
@@ -0,0 +1,15 @@
+import greenfoot.*; 
+
+public class Pumpkin extends Actor
+{
+    public void act()
+    {
+        move(-3);
+        
+        if(getX() < 0)
+        {
+            
+        
+        }
+    }
+}
