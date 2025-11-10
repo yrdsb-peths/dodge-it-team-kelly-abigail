d6f7c19d0e9b3834467674767e2939db271dca41
person-030 <abigailyang783@gmail.com>
2025-11-10 14:22:37 -0500

    random reset position

--- Pumpkin.java
+++ Pumpkin.java
@@ -6,10 +6,27 @@ public class Pumpkin extends Actor
     {
         move(-3);
         
-        if(getX() < 0)
+        if(getX() <= 0)
         {
-            
+            resetPumpkin();
+        }
+    }
+    
+    public void resetPumpkin()
+    {
+        int x = Greenfoot.getRandomNumber(3);
         
+        if(x == 0)
+        {
+            setLocation(600, 60);
+        }
+        else if (x == 1)
+        {
+            setLocation(600, 200);
+        }
+        else
+        {
+            setLocation(600, 350);
         }
     }
 }
